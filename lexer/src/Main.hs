04dc3090df60a97d5b273c564865108bb0bfc9b9
Victor De Ponte <throoze@gmail.com>
2011-05-22 19:03:55 -0430

    Agregado el cliente Main.hs
    Funciona fino, pero hay que cambiar la función analizer para que pueda llamar al lexer. Ademas se hizo un archivito de entrada de prueba llamado prueba.vec

---
+++ lexer/src/Main.hs
@@ -0,0 +1,24 @@
+import IO
+import System
+import System.IO 
+import System.Environment
+--import Lexer 
+
+main = do
+  args <- getArgs
+  case null (args) of
+    False -> do
+      case (length args) < 3 of
+        True -> do
+          case ((args !! 0) == "-e") of
+            True -> do
+              analize (args !! 1)
+            False -> do
+              contents <- readFile (args !! 0)
+              analize contents
+        False -> do
+          putStr "At most two parameters expected\nUSAGE: \tvecti file_to_analize\n\tvecti -e scrap_of_code_to_analize\n"
+    True -> do
+      putStr "At least one parameter expected\nUSAGE: \tvecti file_to_analize\n\tvecti -e scrap_of_code_to_analize\n"
+  
+analize s =  putStrLn s
